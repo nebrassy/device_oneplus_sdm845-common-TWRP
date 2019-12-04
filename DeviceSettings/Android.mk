3d085c0c52b04e677907e26e844bc33e2b414b72
doc HD <doc.divxm@gmail.com>
2019-12-04 10:33:31 +0300

    sdm845-common: devicesettings: Give some more privileges
    Change-Id: Id1dc2b862c34df3857239bb7d5ef4af8a3a49fc9

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -22,6 +22,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := DeviceSettings
 
