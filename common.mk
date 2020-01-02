ed0dc1d311af7b72b0b371805ea7cff2e5182cc7
Simao Gomes Viana <xdevs23@outlook.com>
2020-01-02 15:40:41 +0300

    sdm845-common: Switch to 8 Gb dalvik heap config
    Change-Id: I9e58dd79e2859bd00661835ea30ada027d3a1ee6

--- common.mk
+++ common.mk
@@ -200,4 +200,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oneplus.xml
 
 # setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
